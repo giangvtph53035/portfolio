6b51043c89d5ca1882a231e46fd660153b74265c
giangvtph53035 <giangvtph53035>
2025-09-27 19:23:09 +0700

    Add simple-routes.ts file for Cloudflare deployment

---
+++ resources/js/routes/simple-routes.ts
@@ -0,0 +1,9 @@
+// Simple routes for portfolio - no auth needed
+export const home = () => '/';
+export const projects = () => '/projects';
+
+// Default export
+export default {
+    home,
+    projects,
+};
